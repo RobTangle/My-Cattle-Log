2fe35ca3fcddd2bc622c24f1ef034d77056cf91a
Robert0101 <rob.tangled@gmail.com>
2022-10-21 14:49:48 -0300

    feat/Crear ruta POST para new User "/user"

--- api/src/routes/user/user-routes.ts
+++ api/src/routes/user/user-routes.ts
@@ -11,7 +11,21 @@ router.get("/", async (req, res) => {
     const allUserFromDB = await db.User.findAll();
     return res.status(200).send(allUserFromDB);
   } catch (error: any) {
-    console.log(`Error en ruta "user/". ${error.message}`);
+    console.log(`Error en ruta GET "user/". ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    console.log(`REQ.BODY =`);
+    console.log(req.body);
+    const validatedUser = checkUser(req.body);
+    const newUser = await db.User.create(validatedUser);
+    console.log(newUser.toJSON());
+    return res.status(200).send(newUser);
+  } catch (error: any) {
+    console.log(`Error en ruta POST "user/". ${error.message}`);
     return res.status(400).send({ error: error.message });
   }
 });
