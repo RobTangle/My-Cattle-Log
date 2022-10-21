9dcb44eb57f543a55c80634bb03efc6c6e81fd2e
Robert0101 <rob.tangled@gmail.com>
2022-10-21 14:53:28 -0300

    pro/Agregar router de User como middleware para "/user"

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import testRouter from "./routes/test";
 import animalRouter from "./routes/animal/animal-routes";
+import userRouter from "./routes/user/user-routes";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/test", testRouter);
 app.use("/animal", animalRouter);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   return res.send("HOLA!!!!!!! AA!!");
