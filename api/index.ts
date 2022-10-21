646f85117770450a9b5e94164fb0982f84b8cbb6
Robert0101 <rob.tangled@gmail.com>
2022-10-21 12:37:32 -0300

    CLEAN/Limpiar código viejo o deprecado

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,7 @@
-// const db = require("./src/models/index");
-
 import db from "./src/models";
 const config = require(__dirname + "/src/config/config.js");
 const PORT = config.server.port || 3001;
 const app = require("./src/app");
-
-// app.listen(PORT, () => {
-//   console.log(
-//     "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
-//   );
-//   console.log(`App listening on port ${PORT}`);
-//   console.log(
-//     "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
-//   );
-// });
 
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
