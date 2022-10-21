095b5d192fcf4ca126e6912d1ad1fcd756abfdf7
Robert0101 <rob.tangled@gmail.com>
2022-10-21 14:35:58 -0300

    VAL/Crear function isEmail para chequear formate de email válido

--- api/src/validators/generic-validators.ts
+++ api/src/validators/generic-validators.ts
@@ -116,3 +116,11 @@ export function isTypeofNumber(argumento: any): boolean {
     return false;
   }
 }
+
+//IS EMAIL :
+export function isEmail(argumento: any): boolean {
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  return regex.test(argumento);
+}
