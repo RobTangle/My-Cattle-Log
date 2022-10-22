a824673cd2e2338038fb784bd7b86264eaebc4fe
Robert0101 <rob.tangled@gmail.com>
2022-10-22 20:52:15 -0300

    TSC BUILD

--- api/build/src/app.js
+++ api/build/src/app.js
@@ -21,17 +21,18 @@ app.use("/animal", animal_routes_1.default);
 app.use("/user", user_routes_1.default);
 // for testing:
 app.get("/", (req, res) => {
-    return res.send("HOLA!!!!!!! AA!!");
+    return res.send("Yep! I'm listening your requests...");
 });
 app.get("/auth", jwtMiddleware_1.default, (req, res) => {
     try {
         console.log(`En /auth`);
         console.log(req.auth);
-        return res.send("En /AUTH!");
+        return res.send(`En /AUTH! Tu id de usuario es ${req.auth.sub}`);
     }
     catch (error) {
         return res.status(400).send({ error: error.message });
     }
 });
+//-------
 module.exports = app;
 //! este archivo está siendo importado en index.ts de la raíz
