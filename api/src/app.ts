a1263e88614c51265aa99b8f2bfe6d7fd04e3577
Robert0101 <rob.tangled@gmail.com>
2022-10-22 12:59:29 -0300

    pro/Agregar ruta de prueba "/auth" para testear JWT

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import testRouter from "./routes/test";
 import animalRouter from "./routes/animal/animal-routes";
 import userRouter from "./routes/user/user-routes";
+import jwtCheck from "./config/jwtMiddleware";
 
 const app = express();
 
@@ -12,13 +13,27 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+// routes middlewares:
 app.use("/test", testRouter);
 app.use("/animal", animalRouter);
 app.use("/user", userRouter);
 
+// for testing:
 app.get("/", (req, res) => {
-  return res.send("HOLA!!!!!!! AA!!");
+  return res.send("Yep! I'm listening your requests...");
 });
+
+app.get("/auth", jwtCheck, (req: any, res) => {
+  try {
+    console.log(`En /auth`);
+    console.log(req.auth);
+    return res.send(`En /AUTH! Tu id de usuario es ${req.auth.sub}`);
+  } catch (error: any) {
+    return res.status(400).send({ error: error.message });
+  }
+});
+//-------
 
 module.exports = app;
 //! este archivo está siendo importado en index.ts de la raíz
