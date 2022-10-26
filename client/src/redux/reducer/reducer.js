dfd0b818a5ad5f72df304d5826b8df6533350ac6
Robert0101 <rob.tangled@gmail.com>
2022-10-26 11:59:49 -0300

    build/Create react-redux basic setup

---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "VALUE":
+      break;
+
+    default:
+      break;
+  }
+};
+
+export default rootReducer;
