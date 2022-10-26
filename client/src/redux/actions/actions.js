78229c725d9f240cba3a358cce10f9f5350674a9
Robert0101 <rob.tangled@gmail.com>
2022-10-26 14:28:28 -0300

    actions/Crear setNewAnimalToLoading

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { URL } from "../../constants/urls";
 
 export const CREATE_NEW_ANIMAL = "CREATE_ANIMAL";
+export const SET_NEW_ANIMAL_TO_LOADING = "SET_NEW_ANIMAL_TO_LOADING";
 
 export const createNewAnimal = (obj) => {
   return async function (dispatch) {
@@ -10,6 +11,23 @@ export const createNewAnimal = (obj) => {
       return dispatch({ type: CREATE_NEW_ANIMAL, payload: response.data });
     } catch (error) {
       console.log(`Error en action createAnimal. ${error.message}`);
+    }
+  };
+};
+
+export const setNewAnimalToLoading = () => {
+  return async function (dispatch) {
+    try {
+      return dispatch({
+        type: SET_NEW_ANIMAL_TO_LOADING,
+        payload: { loading: true },
+      });
+    } catch (error) {
+      console.log(`Error en setNewAnimalToLoading. ${error.message}`);
+      return dispatch({
+        type: SET_NEW_ANIMAL_TO_LOADING,
+        payload: { error: error.message },
+      });
     }
   };
 };
