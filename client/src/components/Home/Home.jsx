312cb3f39becef957f2c5879249bce06091f4754
Robert0101 <rob.tangled@gmail.com>
2022-10-27 10:38:21 -0300

    pro/Crear componente Home

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "../NavBar/NavBar";
+export function Home() {
+  return (
+    <>
+      <NavBar />
+      <div>ESTE ES EL HOME! WELCOME!</div>
+    </>
+  );
+}
