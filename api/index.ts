967e391ecf47cf70164de2f7ec95a221757adb2c
Robert0101 <rob.tangled@gmail.com>
2022-10-27 16:42:09 -0300

    sqlz/Cambiar force: true a false

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ const config = require(__dirname + "/src/config/config.js");
 const PORT = config.server.port || 3001;
 const app = require("./src/app");
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
