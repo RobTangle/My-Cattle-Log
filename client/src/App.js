8698343118ea0e4fd5d807b3a7c881033c85f4bf
Robert0101 <rob.tangled@gmail.com>
2022-10-28 01:58:33 -0300

    pro/Agregar componente Management

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CreateNewAnimal } from "./components/CreateNewAnimal";
+import { CreateNewAnimal } from "./components/CreateNewAnimal/CreateNewAnimal";
 import { LandingPage } from "./components/LandingPage";
 import { NavBar } from "./components/NavBar/NavBar";
-import { CardContainer } from "./components/CardContainer";
+import { CardContainer } from "./components/CardContainer/CardContainer";
 import { Home } from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import { SignUp } from "./components/SignUp/SignUp";
+import { Management } from "./components/Management/Management";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
           <Route exact path="/register" element={<SignUp />} />
           {/* <Route path="/home" element={<NavBar />} /> */}
           <Route path="/home" element={<Home />} />
-          <Route exact path="/home/list" element={<CardContainer />} />
+          <Route exact path="/home/management" element={<Management />} />
           <Route exact path="/home/newanimal" element={<CreateNewAnimal />} />
           <Route exact path="/home/profile" element={<Profile />} />
         </Routes>
