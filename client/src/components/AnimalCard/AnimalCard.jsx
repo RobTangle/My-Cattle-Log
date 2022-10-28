71be3d2a316154fc0c6c30f054b4d63fabb20ad1
Robert0101 <rob.tangled@gmail.com>
2022-10-28 03:00:31 -0300

    pro/Codear para dejar funcional ModalEdit

--- client/src/components/AnimalCard/AnimalCard.jsx
+++ client/src/components/AnimalCard/AnimalCard.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { getAllAnimals, deleteAnimal } from "../../redux/actions/actions";
 import "./animalCard.css";
+import { ModalEdit } from "../Modal/ModalEdit";
 
 export function AnimalCard(props) {
   const dispatch = useDispatch();
   const accessToken = localStorage.getItem("tokenCattleTracker");
-
+  const [showValue, setShowValue] = React.useState(false);
   function handleDelete(e) {
     // console.log(e);
     console.log(`Eliminando animal con id ${e.target.value}`);
@@ -15,6 +16,15 @@ export function AnimalCard(props) {
     console.log(animal_id);
     dispatch(deleteAnimal(animal_id, accessToken));
     dispatch(getAllAnimals(accessToken));
+  }
+
+  function handleClickEdit(e) {
+    console.log(`Editando animal con id ${e.target.value}`);
+  }
+
+  function showModal(e) {
+    e.preventDefault();
+    setShowValue(true);
   }
 
   return (
@@ -26,7 +36,11 @@ export function AnimalCard(props) {
       <div className="prop-device-type">{props.animal?.device_type}</div>
       <div className="prop-device-number">{props.animal?.device_number}</div>
       <div className="prop-action">
-        <button className="btn-edit" value={props.animal?.id_senasa}>
+        <button
+          className="btn-edit"
+          value={props.animal?.id_senasa}
+          onClick={showModal}
+        >
           Edit
         </button>
         <button
@@ -36,6 +50,11 @@ export function AnimalCard(props) {
         >
           X
         </button>
+        <ModalEdit
+          show={showValue}
+          setShowValue={setShowValue}
+          animal={props.animal}
+        />
       </div>
     </div>
   );
