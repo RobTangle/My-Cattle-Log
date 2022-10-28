c235d92f2ce3c99650e953f524792bf505d3b46c
Robert0101 <rob.tangled@gmail.com>
2022-10-28 01:57:01 -0300

    pro/Mejorar varios aspectos del componente

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -27,65 +27,77 @@ export function Form() {
     //HACER JS VALIDATIONS...
     dispatch(actions.setNewAnimalToLoading());
     dispatch(actions.createNewAnimal(localState, accessToken));
+    setTimeout(() => {
+      dispatch(actions.getAllAnimals(accessToken));
+    }, 500);
   }
   return (
     <div className="form-modal">
-      <h2>Post a new animal...</h2>
+      <h2>Nuevo animal...</h2>
       <form action="" onSubmit={handleSubmit}>
-        <fieldset className="form-fieldset">
-          <legend>New animal</legend>
-          <div className="inside-form-container">
-            <div className="form-id">
-              <label htmlFor="id_senasa">id senasa</label>
-              <input
-                type="text"
-                name="id_senasa"
-                placeholder="id de senasa"
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="form-type-of-animal">
-              <label htmlFor="type_of_animal">Tipo de animal: </label>
-              <input
-                type="text"
-                name="type_of_animal"
-                placeholder="Ingrese el tipo de animal. Ej: Vaquillona, Novillo, Toro"
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="form-weight_kg">
-              <label htmlFor="weight_kg">Peso: </label>
-              <input
-                type="number"
-                name="weight_kg"
-                placeholder="peso del animal en kilogramos"
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="form-name">
-              <label htmlFor="name">Nombre: </label>
-              <input
-                type="text"
-                name="name"
-                placeholder="nombre del animal"
-                onChange={handleOnChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="device_type">Tipo de dispositivo: </label>
-              <input type="text" name="device_type" onChange={handleOnChange} />
-            </div>
-            <div>
-              <label htmlFor="device_number">Número de dispositivo: </label>
-              <input
-                type="text"
-                name="device_number"
-                onChange={handleOnChange}
-              />
-            </div>
+        {/* <fieldset className="form-fieldset"> */}
+
+        <div className="inside-form-container">
+          <div className="form-id">
+            <label htmlFor="id_senasa">Identificador SENASA *</label>
+            <input
+              type="text"
+              name="id_senasa"
+              placeholder="id de 16 caracteres"
+              maxLength={16}
+              onChange={handleOnChange}
+            />
           </div>
-        </fieldset>
-        <button>Registrar animal</button>
+          <div className="form-type-of-animal">
+            <label htmlFor="type_of_animal">Tipo de animal *</label>
+            <input
+              type="text"
+              name="type_of_animal"
+              placeholder="Ej: Vaquillona / Novillo / Toro"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="form-weight_kg">
+            <label htmlFor="weight_kg">Peso</label>
+            <input
+              type="number"
+              name="weight_kg"
+              placeholder="kilogramos, sin comas"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="form-name">
+            <label htmlFor="name">Nombre *</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="nombre del animal"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="device_type">Tipo de dispositivo * </label>
+            <input
+              type="text"
+              name="device_type"
+              onChange={handleOnChange}
+              placeholder="Ej: Collar / Ear tag"
+            />
+          </div>
+          <div>
+            <label htmlFor="device_number">Número de dispositivo * </label>
+            <input
+              type="text"
+              name="device_number"
+              onChange={handleOnChange}
+              placeholder="código de 8 caracteres"
+              maxLength={8}
+            />
+          </div>
+
+          <button>Registrar animal</button>
+        </div>
+        {/* </fieldset> */}
       </form>
     </div>
   );
