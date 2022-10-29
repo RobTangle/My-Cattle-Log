8eede53bb27145018c41abbf0b8ea0652f165dc4
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-29 12:06:54 -0300

    config theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color_bg: "#2a2a2e",
+        white: "#ffffff",
+        brown: "#5C3D2E",
+        light_brown: "#865439",
+        green: "#146356",
+        light_green: "#A3DA8D",
+        beige: "#F3C892",
+        color_light: "#FFF1BD",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
