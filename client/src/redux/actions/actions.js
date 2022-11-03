8d5d6401f83ca7df6ac7a932ba20df85a48db60a
Robert0101 <rob.tangled@gmail.com>
2022-11-03 14:38:11 -0300

    pro/Crear types y actions para Note

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -29,6 +29,10 @@ import {
   GET_PREGNANT_ASC,
   CLEAN_PREGNANT_ASC,
   SET_PREGNANT_ASC_TO_LOADING,
+  GET_NOTES_FROM_USER,
+  POST_NEW_NOTE,
+  DELETE_NOTE,
+  UPDATE_NOTE,
 } from "./types";
 import { header } from "../../constants/token";
 
@@ -367,6 +371,81 @@ export function cleanPregnantAsc() {
       dispatch({ type: CLEAN_PREGNANT_ASC, payload: { pure: true } });
     } catch (error) {
       dispatch({ type: CLEAN_PREGNANT_ASC, payload: { error: error.message } });
+    }
+  };
+}
+
+export function getNotesFromUser(token) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(URL + "note/all", header(token));
+      dispatch({
+        type: GET_NOTES_FROM_USER,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_NOTES_FROM_USER,
+        payload: { error: error.message },
+      });
+    }
+  };
+}
+
+export function postNewNote(newNoteObj, token) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.post(
+        URL + "note/newNote",
+        newNoteObj,
+        header(token)
+      );
+      dispatch({
+        type: POST_NEW_NOTE,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: POST_NEW_NOTE,
+        payload: { error: error.message },
+      });
+    }
+  };
+}
+
+export function deleteNote(noteId, token) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.delete(
+        URL + `note/${noteId}`,
+        header(token)
+      );
+      dispatch({
+        type: DELETE_NOTE,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: DELETE_NOTE,
+        payload: { error: error.message },
+      });
+    }
+  };
+}
+
+export function updateNote(noteObj, token) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.put(URL + "note/", noteObj, header(token));
+      dispatch({
+        type: UPDATE_NOTE,
+        action: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_NOTE,
+        action: { error: error.message },
+      });
     }
   };
 }
