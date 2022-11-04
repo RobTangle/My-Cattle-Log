80b7b53a398c106f2a875bb248541a1e48e6b296
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-04 09:33:52 -0300

    text

--- client/src/components/Note/NoteComponent.jsx
+++ client/src/components/Note/NoteComponent.jsx
@@ -34,7 +34,7 @@ export function NoteComponent() {
       </h2>
       <div>
         <button
-          className="border border-solid border-transparent bg-green px-3 py-1 rounded-sm  hover:bg-white hover:text-green hover:border-green transition-all ease-in-out duration-500 text-green flex items-center gap-3"
+          className="border border-solid border-transparent bg-green px-3 py-1 rounded-sm text-white hover:bg-white hover:text-green hover:border-green transition-all ease-in-out duration-500 flex items-center gap-3"
           onClick={toggleNoteForm}
         >
           <IoMdAddCircleOutline />
